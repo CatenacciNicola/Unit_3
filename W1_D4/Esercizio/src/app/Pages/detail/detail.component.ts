979e322f561fbe9a79b963f1d593067d347bdd24
Catenacci Nicola <catenaccinicola@outlook.it>
2024-05-23 18:03:53 +0200

    Dettagli

--- W1_D4/Esercizio/src/app/Pages/detail/detail.component.ts
+++ W1_D4/Esercizio/src/app/Pages/detail/detail.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
+import { IJsonContent } from '../../Models/i-jason-content';
+import { iPost } from './../../Models/ipost';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './detail.component.scss'
 })
 export class DetailComponent {
+  currentPost!:iPost
+
+  constructor(
+    private route:ActivatedRoute)
+  {}
+
+  ngOnInit(){
+    this.route.params.subscribe((params:any)=>{
+      this.getPost(params.id)
+    })
+
+  }
+
+  async getPost(id:number){
+    const response=await fetch("../../../assets/db.json")
+    const jsonContent=<IJsonContent> await response.json()
+    const postTrovato=jsonContent.posts.find(post => post.id == id)
+    if(postTrovato){
+      this.currentPost=postTrovato
+    }
+  }
+
+  goBack(){
+    history.back();
+  }
 
 }
--- W1_D4/Esercizio/src/app/app-routing.module.ts
+++ W1_D4/Esercizio/src/app/app-routing.module.ts
@@ -4,19 +4,25 @@ import { HomeComponent } from './Pages/home/home.component';
 import { ActivePostsComponent } from './Pages/active-posts/active-posts.component';
 import { InactivePostsComponent } from './Pages/inactive-posts/inactive-posts.component';
 import { Page404Component } from './Pages/page404/page404.component';
+import { DetailComponent } from './Pages/detail/detail.component';
 
 const routes: Routes = [
   {path:"",
     component:HomeComponent,
-    title:"Home"
+    title:"Home",
+    pathMatch:"full"
   },
   {path:"active-post",
     component:ActivePostsComponent,
     title:"active-posts"
   },
   {path:"inactive-post",
-    component:InactivePostsComponent,
-    title:"inactive-posts"
+  component:InactivePostsComponent,
+  title:"inactive-posts"
+  },
+  {path:"details/:id",
+   component:DetailComponent,
+   title:"details"
   },
   {path:"**",
     component:Page404Component,
