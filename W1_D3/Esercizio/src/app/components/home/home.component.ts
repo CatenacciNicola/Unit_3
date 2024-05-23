5894e0f5bc3d6e3c7441069cae64a1808d5be9a7
Catenacci Nicola <catenaccinicola@outlook.it>
2024-05-23 11:38:22 +0200

    Correzione Esercizio

--- W1_D3/Esercizio/src/app/components/home/home.component.ts
+++ W1_D3/Esercizio/src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
+
 import { Component } from '@angular/core';
-import {iPost} from '.../Interface/post'
+import { iPost } from '../../Interface/post';
+import {IJsonContent} from '../../Interface/i-json-content'
+
 
 @Component({
   selector: 'app-home',
@@ -8,14 +11,32 @@ import {iPost} from '.../Interface/post'
 })
 export class HomeComponent {
 
-  apiUrl:string=".../assets/db.json"
-
-  postArr:iPost[]=[]
+  postsArr:iPost[]=[]
+  firstPost!:iPost;
+  randomPosts:iPost[]=[];
 
   ngOnInit(){
-
+    this.getPosts()
   }
 
+  async getPosts(){
+    const response=await fetch("../../../assets/db.json")
+    const posts=<IJsonContent> await response.json()
+    this.postsArr=posts.posts
+    let firstPost=this.getFirstPost()
+    if (firstPost){
+      this.firstPost=firstPost
+    }
+    this.randomPosts=this.getRandomPosts()
+  }
 
+  getFirstPost(){
+    return this.postsArr.shift()
+  }
+
+  getRandomPosts(): iPost[] {
+    const shuffled = [...this.postsArr].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 4);
+  }
 
 }
