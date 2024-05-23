e97e6d3111c7371fdfe8c4b6086ce8ff1b84ae90
Catenacci Nicola <catenaccinicola@outlook.it>
2024-05-23 16:52:31 +0200

    Post inattivi

--- W1_D4/Esercizio/src/app/Pages/active-posts/active-posts.component.ts
+++ W1_D4/Esercizio/src/app/Pages/active-posts/active-posts.component.ts
@@ -23,7 +23,7 @@ export class ActivePostsComponent {
 
     console.log("Tutti i posts",postsArr)
     this.activeArr=postsArr.filter(post=>post.active===true)
-
+    console.log("Post attivi", this.activeArr)
   }
 
 }
--- W1_D4/Esercizio/src/app/Pages/inactive-posts/inactive-posts.component.ts
+++ W1_D4/Esercizio/src/app/Pages/inactive-posts/inactive-posts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { iPost } from '../../Models/ipost';
+import { IJsonContent } from '../../Models/i-jason-content';
 
 @Component({
   selector: 'app-inactive-posts',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './inactive-posts.component.scss'
 })
 export class InactivePostsComponent {
+  inactiveArr:iPost[]=[]
+  ngOnInit(){
+    this.getInactivePosts()
+  }
 
+  async getInactivePosts(){
+    const response=await fetch("../../../assets/db.json")
+    const posts=<IJsonContent>await response.json()
+    this.inactiveArr=(posts.posts).filter(post=>post.active===false)
+    console.log("Post inattivi", this.inactiveArr)
+  }
 }
